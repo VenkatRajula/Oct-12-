0dfd609648d90b1221f5fe94f44af457f58a96ad
VenkatRajula <venkatrajula77@gmail.com>
2021-10-12 05:46:03 +0000

    modified App.java

---
+++ src/main/java/com/tcs/App.java
@@ -0,0 +1,14 @@
+package com.tcs;
+
+/**
+ * Hello world!
+ * Welcome to Devops!!!!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
